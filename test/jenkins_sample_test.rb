ee14696ca00ef50a679590b35fb9973e106f75d3
madebayo <prozpris@gmail.com>
2019-04-10 16:12:41 -0700

    Update jenkins_sample_test.rb

--- test/jenkins_sample_test.rb
+++ test/jenkins_sample_test.rb
@@ -9,7 +9,7 @@ class JenkinsSampleTest < MiniTest::Unit::TestCase
     uri_params = {
       :host => ENV['TEST_IP_ADDRESS'] || 'localhost',
       :port => (ENV['TEST_PORT'] || '80').to_i,
-      :path => './index.html'
+      :path => 'index.html'
     }
     @webpage = Net::HTTP.get(URI::HTTP.build(uri_params))
   end
